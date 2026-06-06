9bf093034ff3d1ab200e7d4ac105a16a2f823978
alfi26-star <alfilr2026@gmail.com>
2026-06-06 23:57:02 +0530

    Initial CSV Analyzer

---
+++ analyzer.py
@@ -0,0 +1,17 @@
+print("Hello World")
+import pandas as pd
+
+# Load data
+df = pd.read_csv("sample_data.csv")
+
+print("\nDataset Shape:")
+print(df.shape)
+
+print("\nColumns:")
+print(df.columns.tolist())
+
+print("\nMissing Values:")
+print(df.isnull().sum())
+
+print("\nSummary Statistics:")
+print(df.describe())
